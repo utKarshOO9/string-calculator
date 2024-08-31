1372ea31ba5a7c7937279cd2695592dff661abcc
utKarsh009 <utkarshsrivastava994@gmail.com>
2024-08-31 22:04:47 +0530

    fix: enhance addition method for addition

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,12 +1,25 @@
+import re
+
+DELIMITER_REGEX = re.compile(r"(//\S\n)")
 
 def add(numbers: str) -> int:
     if numbers.strip() == "":
         return 0
+    
+    delimiter = "\n"
+    match = DELIMITER_REGEX.search(numbers)
+    
+    if match is not None:
+        numbers = DELIMITER_REGEX.sub("", numbers)
+        delimiter = match.group(1).replace("//", "").strip()
+        
     result = 0
-    numbers = numbers.replace('\n', ',')
+    numbers = numbers.replace(delimiter, ',')
     numbers = numbers.split(',')
+
     for i in numbers:
         if i.strip() == "":
             continue
         result += int(i)
+    
     return result

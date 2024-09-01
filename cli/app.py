a4c179530146e9f251d6a9be4342abb7e8ea6bab
utKarsh009 <utkarshsrivastava994@gmail.com>
2024-09-01 11:59:35 +0530

    fix: CI to install poetry packages

--- cli/app.py
+++ cli/app.py
@@ -1,13 +1,20 @@
 import click
 from string_calculator import calculator
 
+
 @click.command("string_calculator")
 @click.option("--numbers", required=True, help="strings of number with delimited values")
 def main(numbers: str):
     result = calculator.add(numbers)
     click.echo(
         "\n".join(
-            ["Output:", "==================", f"Input: {numbers}", f"Addition of numbers: {result}", "=================="]
+            [
+                "Output:",
+                "==================",
+                f"Input: {numbers}",
+                f"Addition of numbers: {result}",
+                "==================",
+            ]
         )
     )
 

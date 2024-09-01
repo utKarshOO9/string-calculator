206cdc5bdb42613be30d0d071dc7b2ed030ef4e8
utKarsh009 <utkarshsrivastava994@gmail.com>
2024-09-01 17:18:14 +0530

    feat: initialize fastapi app

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,7 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 import logging
+from api import main
 
 logger = logging.getLogger(__file__)
 
@@ -6,5 +9,6 @@ logger = logging.getLogger(__file__)
 class TestCalculatorApi:
 
     def test_setup_api(self):
-
-        pass
+        logger.info("Running test case for method check in module")
+        assert hasattr(main, "api")
+        assert isinstance(main.api, FastAPI)

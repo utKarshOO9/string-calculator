825cf77c567a58c1eb19d389000b1b59c2163851
utKarsh009 <utkarshsrivastava994@gmail.com>
2024-09-01 11:55:58 +0530

    feat: cli app handling with numbers calculations

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import pytest
 from click.testing import CliRunner
 from cli import app
 import logging
@@ -11,7 +12,7 @@ def build_text_format(numbers, sum_val):
         "==================",
         f"Input: {numbers}",
         f"Addition of numbers: {sum_val}",
-        "=================="
+        "==================",
     ]
 
 
@@ -29,7 +30,6 @@ class TestCliApp:
             result = runner.invoke(app.main, ["--numbers", ""])
             assert result.exit_code == 0
             assert result.output.strip() == "\n".join(build_text_format("", 0))
-    
 
     def test_cli_app_accepts_number_parameters_with_delimited_numbers(self):
         runner = CliRunner()
@@ -37,3 +37,16 @@ class TestCliApp:
             result = runner.invoke(app.main, ["--numbers", "1,2"])
             assert result.exit_code == 0
             assert result.output.strip() == "\n".join(build_text_format("1,2", 3))
+
+    @pytest.mark.parametrize("_desc,param,output", [
+        ("addition-numbers-with-comma-formatted", "1,2,3,4", 10),
+        ("addition-numbers-with-\n-comma-formatted", "1\n2,3,4", 10),
+        ("addition-numbers-with-//$\n-formatted", "//$\n1$2$3$4", 10)
+        ])
+    def test_cli_app_perform_addition_with_multiple_numbers(self, _desc: str, param: str, output: int):
+        runner = CliRunner()
+        logger.info(f"Executing test for {_desc}")
+        with runner.isolation():
+            result = runner.invoke(app.main, ["--numbers", param])
+            assert result.exit_code == 0
+            assert result.output.strip() == "\n".join(build_text_format(param, output))

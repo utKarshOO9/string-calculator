9251be378fa47a68a6800489a624300c839f3f3e
utKarsh009 <utkarshsrivastava994@gmail.com>
2024-09-01 18:04:46 +0530

    feat: added calculator addition to api

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from api.model import CalculatorRequest, CalculatorResponse
+from string_calculator import calculator as calc
 
 api = FastAPI()
 
 
 @api.post("/calculator", response_model=CalculatorResponse)
 async def calculator(req: CalculatorRequest):
-    return CalculatorResponse(input=req.numbers, output=0, errors=[])
+    result = calc.add(req.numbers)
+    return CalculatorResponse(input=req.numbers, output=result, errors=[])

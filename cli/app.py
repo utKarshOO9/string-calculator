210905ae6c87dbc75df9976215801be7a16cc159
utKarsh009 <utkarshsrivastava994@gmail.com>
2024-09-01 11:46:24 +0530

    feat: add cli feature & update code formats

--- cli/app.py
+++ cli/app.py
@@ -1,3 +1,15 @@
+import click
 
-def main():
-    pass
+
+@click.command("string_calculator")
+@click.option("--numbers", required=True, help="strings of number with delimited values")
+def main(numbers: str):
+    click.echo(
+        "\n".join(
+            ["Output:", "==================", f"Input: {numbers}", "Addition of numbers: 0", "=================="]
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,17 +6,16 @@ logger = logging.getLogger(__file__)
 
 
 def build_text_format(numbers, sum_val):
-    return f"""
-        Output: 
-        ==================
-        Input: {numbers}
-        Addition of numbers: {sum_val}
-        ==================
-    """
+    return [
+        "Output:",
+        "==================",
+        f"Input: {numbers}",
+        f"Addition of numbers: {sum_val}",
+        "=================="
+    ]
 
 
 class TestCliApp:
-   
     def test_has_main_method(self):
         """
         test to check if calculator has add function or not
@@ -26,6 +25,7 @@ class TestCliApp:
 
     def test_cli_app_accepts_number_parameters(self):
         runner = CliRunner()
-        result = runner.invoke(app, ['--numbers', ''])
-        assert result.exit_code == 0
-        assert result.output == build_text_format('', 0)
+        with runner.isolation():
+            result = runner.invoke(app.main, ["--numbers", ""])
+            assert result.exit_code == 0
+            assert result.output.strip() == "\n".join(build_text_format("", 0))
